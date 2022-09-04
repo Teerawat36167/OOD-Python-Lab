f65c20b3ec9e68033e3286ed901084cbf06390d8
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2022-09-04 21:12:18 +0700

    Chapter 6

--- Chapter 6/GCD.py
+++ Chapter 6/GCD.py
@@ -6,13 +6,13 @@ def gcd(a, b) :
 
 a, b = input("Enter Input : ").split(" ")
 
-if int(b) < 0 and int(a) == 0:
-     a, b = a, b
-elif abs(int(b)) > abs(int(a)):
+if abs(int(a)) < abs(int(b)) :
      a, b = b, a
 
 if int(a) == 0 and int(b) == 0 :
      print("Error! must be not all zero.")
 else :
      ans = abs(gcd(int(a),int(b)))
+     if int(b) == 0 and int(a) < 0 :
+          a, b = b, a
      print(f'The gcd of {a} and {b} is : {ans}')
--- Chapter 6/temp.py
+++
@@ -1,4 +0,0 @@
-if abs(int(input[0])) > abs(int(input[1])) :
-     a, b = int(input[0]), int(input[1])
-elif abs(int(input[0])) <= abs(int(input[1])) :
-     a, b = int(input[1]), int(input[0])
