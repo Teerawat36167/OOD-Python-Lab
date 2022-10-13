e86b93e6c85a0b15ba9a6d3091cf04646d995600
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2022-10-13 12:21:08 +0700

    Chapter 7 Program 4

--- Chapter 7/deleteNodeInTree.py
+++ Chapter 7/deleteNodeInTree.py
@@ -13,11 +13,59 @@ class BinarySearchTree:
         self.root = None
 
     def insert(self, val):  
-        pass
+        newData = Node(val)
+        if self.root is None :
+            self.root = newData
+        else :
+            cur = self.root
+            while True :
+                if cur.data > val :
+                    if cur.left is None :
+                        cur.left = newData
+                        break
+                    else :
+                        cur = cur.left
+                elif cur.data <= val :
+                    if cur.right is None :
+                        cur.right = newData
+                        break
+                    else :
+                        cur = cur.right
+        return self.root
 
-    def delete(self,r, data):
-        pass
-                
+    def delete(self, r, data):
+        if r is None: 
+            print("Error! Not Found DATA")
+            return
+
+        if self.root.left == None and self.root.right == None and self.root.data == data:
+            self.root = None
+        elif self.root.left == None and self.root.data == data:
+            self.root = self.root.right
+        elif self.root.right == None and self.root.data == data:
+            self.root = self.root.left
+
+        if r.data != data :
+            if r.data > data :
+                r.left = self.delete(r.left, data)
+            elif r.data < data :
+                r.right = self.delete(r.right, data) 
+        elif r.data == data :
+            if r.left == None :
+                r = r.right
+                return r
+            elif r.right == None :
+                r = r.left
+                return r
+            else:
+                current = r.right
+                while current.left != None :
+                    current = current.left
+                r.data = current.data 
+                r.right = self.delete(r.right, current.data)
+
+        return r
+            
 def printTree90(node, level = 0):
     if node != None:
         printTree90(node.right, level + 1)
@@ -27,4 +75,14 @@ def printTree90(node, level = 0):
 
 tree = BinarySearchTree()
 data = input("Enter Input : ").split(",")
-#code here
+
+for i in data :
+    inp = i.split(" ")
+    if inp[0] == 'i' :
+        tree.insert(int(inp[1]))
+        print(f'insert {inp[1]}')
+        printTree90(tree.root)
+    elif inp[0] == 'd' :
+        print(f'delete {inp[1]}')
+        tree.delete(tree.root,int(inp[1]))
+        printTree90(tree.root)
