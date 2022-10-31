d6c086e4d427ea4808937ede9ec90acddd5c6586
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2022-10-31 23:16:40 +0700

    Chapter 9 Program 2

--- Chapter 9/straightSelectionSort(recursive).py
+++ Chapter 9/straightSelectionSort(recursive).py
@@ -1,4 +1,22 @@
-def straightSelectionSort():
-     pass
+def maxIndex(list, i, j):
+     if i == j:
+          return i
+     k = maxIndex(list, i + 1, j)
+     return (k if list[i] < list[k] else i)
+
+def straightSelectionSort(list, n):
+     if n == 0:
+          return
+     k = maxIndex(list, 0, n)
+     if k != n :
+          temp = list[k]
+          list[k] = list[n]
+          list[n] = temp
+          print(f'swap {list[k]} <-> {list[n]} : {list}')
+     straightSelectionSort(list, n-1)
 
 inp = [int(i) for i in input("Enter Input : ").split()]
+
+straightSelectionSort(inp, len(inp)-1)
+
+print(inp)
