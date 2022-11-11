f00cfe56861132c53ee04371e9c625052705050c
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2022-11-12 01:46:41 +0700

    update

---
+++ Chapter 10/BinarySearch.py
@@ -0,0 +1,6 @@
+def bi_search(l, r, arr, x):
+    pass
+
+inp = input('Enter Input : ').split('/')
+arr, k = list(map(int, inp[0].split())), int(inp[1])
+print(bi_search(0, len(arr) - 1, sorted(arr), k))
--- Chapter 9/sortSubset.py
+++ Chapter 9/sortSubset.py
@@ -1,5 +1,34 @@
-inp = input("Enter Input : ").split('/')
-ans = inp[0]
-set = [int(i) for i in inp[1].split()]
+def combination(arr):
+    def findSum(arr, n):
+        if n == 0:
+            return [[]]
+        list = []
+        for i in range(len(arr)):
+            start = arr[i]
+            rem = arr[i + 1 :]
+            for j in findSum(rem, n - 1):
+                print([start])
+                print(j)
+                list.append(sortList([start] + j))
+        return sortList(list)
 
-print(f'[{ans}]')
+    res = list()
+    for i in range(1, len(arr) + 1):
+        res.append(findSum(arr, i))
+    return res
+
+def sortList(list):
+    for i in range(len(list)-1):
+        for j in range(i+1,len(list)):
+            if int(list[i]) > int(list[j]) :
+                temp = list[i]
+                list[i] = list[j]
+                list[j] = temp
+    return list
+
+inp = input("Enter Input : ").split('/')
+ans = int(inp[0])
+inp = [int(i) for i in inp[1].split()]
+subList = combination(inp)
+subSet = []
+print(subList)
