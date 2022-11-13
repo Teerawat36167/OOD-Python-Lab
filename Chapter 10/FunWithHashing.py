91f139d64ab4b5212b4b2b4ba75e2259a6601962
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2022-11-14 00:35:07 +0700

    update

--- Chapter 10/FunWithHashing.py
+++ Chapter 10/FunWithHashing.py
@@ -7,5 +7,38 @@ class Data:
           return "({0}, {1})".format(self.key, self.value)
 
 class hash:
+     def __init__(self,size,maxCol):
+          self.table = [None]*size
+          self.size = size
+          self.maxCol = maxCol
 
-     pass
+     def insert(self,key,data):
+          if self.isfull() :
+               print("This table is full !!!!!!")
+               exit(0)
+          else:
+               index = 0
+               for i in key:
+                    index += i
+               index %= self.size
+
+               if self.table[index] == None:
+                    self.table[index] = Data(key,data)
+               else:
+                    col = 0
+                    newIndex = index
+                    print(f'collision number {col+1} at {index}')
+
+                    #Not Finish
+
+     def isfull(self):
+          return None not in self.table
+
+print(" ***** Fun with hashing *****")
+inp = input("Enter Input : ").split('/')
+size, maxCol = inp[0].split()
+H = hash(int(size),int(maxCol))
+data = inp[1].split(",")
+
+for i in data:
+     H.insert(i.split()[0],i.split()[1])
